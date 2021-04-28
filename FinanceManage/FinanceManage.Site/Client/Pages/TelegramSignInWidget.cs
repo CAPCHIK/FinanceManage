7781844501afc9fd549efaa6ccd18559523ce4bd
Maksim Makushchenko <maksalmak@gmail.com>
2021-04-28 13:23:03 +0300

    Add auth via telegram

---
+++ FinanceManage/FinanceManage.Site/Client/Pages/TelegramSignInWidget.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinanceManage.Site.Client.Pages
+{
+    public class TelegramSignInWidget : ComponentBase
+    {
+        [Inject]
+        public IConfiguration Configuration { get; set; }
+        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        {
+            base.BuildRenderTree(builder);
+            builder.OpenElement(0, "script");
+            builder.AddAttribute(1, "async");
+            builder.AddAttribute(2, "src", "https://telegram.org/js/telegram-widget.js?14");
+            builder.AddAttribute(2, "data-telegram-login", Configuration.GetValue<string>("TelegramBotOptions:UserName"));
+            builder.AddAttribute(3, "data-size", "large");
+            builder.AddAttribute(4, "data-userpic", "false");
+            builder.AddAttribute(5, "data-onauth", "onTelegramAuth(user)");
+            builder.CloseElement();
+        }
+    }
+}
+
+//< script async src = "https://telegram.org/js/telegram-widget.js?14" data-telegram-login="fin_manage_dev_bot" data-size="small" data-userpic="false" data-onauth="onTelegramAuth(user)"></script>
+
---
+++ FinanceManage/FinanceManage.Site/Client/TelegramAuthListener.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace FinanceManage.Site.Client
+{
+    public class TelegramAuthListener
+    {
+        public static event Action<TelegramUserInfo> UserAuthenticated;
+        [JSInvokable]
+        public static void UserAuthCallback(TelegramUserInfo user)
+        {
+            Console.WriteLine("auth done");
+            UserAuthenticated?.Invoke(user);
+        }
+
+        public class TelegramUserInfo
+        {
+            [JsonPropertyName("id")]
+            public int Id { get; set; }
+            [JsonPropertyName("first_name")]
+            public string FirstName { get; set; }
+            [JsonPropertyName("last_name")]
+            public string LastName { get; set; }
+            [JsonPropertyName("username")]
+            public string UserName { get; set; }
+            [JsonPropertyName("photo_url")]
+            public string PhotoUrl { get; set; }
+            [JsonPropertyName("auth_date")]
+            public int AuthDate { get; set; }
+            [JsonPropertyName("hash")]
+            public string Hash { get; set; }
+        }
+    }
+}

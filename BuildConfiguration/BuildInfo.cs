eea651e2ac420ebcca76af547be411aa6a2be1d4
Maksim Makushchenko <maksalmak@gmail.com>
2021-07-30 19:49:28 +0300

    Fix build script

--- BuildConfiguration/BuildInfo.cs
+++ BuildConfiguration/BuildInfo.cs
@@ -4,6 +4,13 @@ namespace BuildConfiguration
 {
     public class BuildInfo
     {
+        public BuildInfo() {}
+        public BuildInfo(DateTimeOffset date, string buildId)
+        {
+            Date = date;
+            BuildId = buildId;
+        }
+
         public DateTimeOffset Date { get; set; }
         public string BuildId { get; set; }
     }

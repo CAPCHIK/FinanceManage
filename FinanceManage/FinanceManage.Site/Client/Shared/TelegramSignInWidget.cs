678c2ef192c9305c4adc82191e15e64512e1df0a
Maksim Makushchenko <maksalmak@gmail.com>
2021-07-30 20:06:01 +0300

    Fix telegram bot username

--- FinanceManage/FinanceManage.Site/Client/Shared/TelegramSignInWidget.cs
+++ FinanceManage/FinanceManage.Site/Client/Shared/TelegramSignInWidget.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using FinanceManage.Site.Client.Models.Options;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Rendering;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,7 +13,7 @@ namespace FinanceManage.Site.Client.Shared
     public class TelegramSignInWidget : ComponentBase
     {
         [Inject]
-        public IConfiguration Configuration { get; set; }
+        public IOptions<TelegramBotInfo> Configuration { get; set; }
 
         [Parameter]
         public string Size { get; set; } = "large";
@@ -22,7 +24,7 @@ namespace FinanceManage.Site.Client.Shared
             builder.OpenElement(0, "script");
             builder.AddAttribute(1, "async");
             builder.AddAttribute(2, "src", "https://telegram.org/js/telegram-widget.js?14");
-            builder.AddAttribute(2, "data-telegram-login", Configuration.GetValue<string>("TelegramBotOptions:UserName"));
+            builder.AddAttribute(2, "data-telegram-login", Configuration.Value.UserName);
             builder.AddAttribute(3, "data-size", Size);
             builder.AddAttribute(4, "data-userpic", "false");
             builder.AddAttribute(5, "data-onauth", "onTelegramAuth(user)");

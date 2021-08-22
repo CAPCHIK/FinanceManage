0d5de7aec91b9ec3fec0eb2d8082720906f0350a
Maksim Makushchenko <maksalmak@gmail.com>
2021-08-22 23:38:06 +0300

    Add creating purchase modal on site

---
+++ FinanceManage/FinanceManage.CQRS.Handlers.Server/GetAvailableCategoriesForChatHandler.cs
@@ -0,0 +1,37 @@
+﻿using FinanceManage.Database;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+using static FinanceManage.CQRS.Queries.GetAvailableCategoriesForChat;
+
+
+namespace FinanceManage.CQRS.Handlers.Server
+{
+    class GetAvailableCategoriesForChatHandler : IRequestHandler<Command, List<string>>
+    {
+        private FinanceManageDbContext dbContext;
+
+        public GetAvailableCategoriesForChatHandler(FinanceManageDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public async Task<List<string>> Handle(Command request, CancellationToken cancellationToken)
+        {
+            var categories = await dbContext
+                .Purchases
+                .Where(p => p.TelegramChatId == request.ChatId)
+                .GroupBy(p => p.Category)
+                .Select(g => new { Category = g.Key, Count = g.Count() })
+                .OrderByDescending(p => p.Count)
+                .Select(p => p.Category)
+                .ToListAsync(cancellationToken);
+            return categories;
+        }
+    }
+}
--- FinanceManage/FinanceManage.Site/Server/Controllers/PurchasesController.cs
+++ FinanceManage/FinanceManage.Site/Server/Controllers/PurchasesController.cs
@@ -12,6 +12,7 @@ namespace FinanceManage.Site.Server.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class PurchasesController : ControllerBase
     {
         private readonly IMediator mediator;
@@ -21,7 +22,6 @@ namespace FinanceManage.Site.Server.Controllers
             this.mediator = mediator;
         }
 
-        [Authorize]
         [HttpGet("{chatId:long}")]
         public async Task<ActionResult<ListWrapper<GetPurchases.Response>>> Get(
             [FromRoute] long chatId,
@@ -37,9 +37,26 @@ namespace FinanceManage.Site.Server.Controllers
 
             return await mediator.Send(command with { ChatId = chatId });
         }
-        [Authorize]
+
+        [HttpPost("{chatId:long}")]
+        public async Task<ActionResult<bool>> CreatePurchase(
+            [FromRoute] long chatId,
+            [FromBody] SavePurchase.Command command)
+        {
+            var userId = int.Parse(User.Claims.Single(c => c.Type == ClaimTypes.NameIdentifier).Value);
+            var userHasAccess = await mediator.Send(
+               new GetUserHasAccessToChat.Command(userId, chatId));
+
+            if (!userHasAccess)
+            {
+                return Forbid();
+            }
+
+            return await mediator.Send(command with { BuyerTelegramId = userId, TelegramChatId = chatId } );
+        }
+
         [HttpPut("{purchaseId:guid}")]
-        public async Task<ActionResult<bool>> Get(
+        public async Task<ActionResult<bool>> UpdatePurchase(
             [FromRoute] Guid purchaseId,
             [FromBody] UpdatePurchase.Command command)
         {
@@ -52,6 +69,20 @@ namespace FinanceManage.Site.Server.Controllers
             }
 
             return await mediator.Send(command with { PurchaseId = purchaseId });
+        }
+
+        [HttpGet("categories/{chatId:long}")]
+        public async Task<ActionResult<List<string>>> GetCategories(long chatId)
+        {
+            var userHasAccess = await mediator.Send(
+               new GetUserHasAccessToChat.Command(int.Parse(User.Claims.Single(c => c.Type == ClaimTypes.NameIdentifier).Value), chatId));
+
+            if (!userHasAccess)
+            {
+                return Forbid();
+            }
+
+            return await mediator.Send(new GetAvailableCategoriesForChat.Command(chatId));
         }
     }
 }
